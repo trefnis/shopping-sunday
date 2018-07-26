fe7aca5b87b104693190bd7e4e63bb9f6d8e0629
Michał Olchawa <michal.olchawa@netguru.pl>
2018-07-26 09:49:52 +0200

    Update meta information.

--- server/api/server.js
+++ server/api/server.js
@@ -11,7 +11,10 @@ const reminders = require('./routes/reminders');
 dotenv.config();
 const app = express();
 
-app.use(logger('dev'));
+if (process.env.NODE_ENV !== 'production') {
+  app.use(logger('dev'));
+}
+
 app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 app.use(cookieParser());
 app.use(express.json());
